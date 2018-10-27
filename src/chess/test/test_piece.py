fade5e963450a772a8e3a0bfeeeaf54b93afbfba
Joshua Langley <jlangley29@bloomberg.com>
2018-10-27 11:50:53 +0100

    Add additional tests to check for the existence of all moves

--- src/chess/test/test_piece.py
+++ src/chess/test/test_piece.py
@@ -53,3 +53,48 @@ def test_symbol_for(piece, expected):
 )
 def test_move_count_for(piece, expected):
     assert len(piece.moves) == expected, f"{str(piece)} should have {expected} moves, but had {len(piece.moves)} moves."
+
+@pytest.mark.parametrize( "piece,x,y,directional,can_take",
+    [
+        (Pawn("Black"), -1, 1, False,True),
+        (Pawn("Black"), 1, 1, False,True),
+        (Pawn("Black"), 0, 1, False,False),
+        (Knight("Black"), 1, 3, False,True),
+        (Knight("Black"), -1, 3, False,True),
+        (Knight("Black"), 1, -3, False,True),
+        (Knight("Black"), -1, -3, False,True),
+        (Knight("Black"), 3, 1, False,True),
+        (Knight("Black"), -3, 1, False,True),
+        (Knight("Black"), 3, -1, False,True),
+        (Knight("Black"), -3, -1, False,True),
+        (Rook("Black"), 0, 1, True,True),
+        (Rook("Black"), 1, 0, True,True),
+        (Rook("Black"), 0, -1, True,True),
+        (Rook("Black"), -1, 0, True,True),
+        (Bishop("Black"), 1, 1, True,True),
+        (Bishop("Black"), -1, 1, True,True),
+        (Bishop("Black"), 1, -1, True,True),
+        (Bishop("Black"), -1, -1, True,True),
+        (Queen("Black"), 0, 1, True,True),
+        (Queen("Black"), 0, -1, True,True),
+        (Queen("Black"), 1, 0, True,True),
+        (Queen("Black"), -1, 0, True,True),
+        (Queen("Black"), 1, 1, True,True),
+        (Queen("Black"), 1, -1, True,True),
+        (Queen("Black"), -1, 1, True,True),
+        (Queen("Black"), -1, -1, True,True),
+        (King("Black"), 0, 1, False,True),
+        (King("Black"), 0, -1, False,True),
+        (King("Black"), 1, 0, False,True),
+        (King("Black"), -1, 0, False,True),
+        (King("Black"), 1, 1, False,True),
+        (King("Black"), 1, -1, False,True),
+        (King("Black"), -1, 1, False,True),
+        (King("Black"), -1, -1, False,True)
+
+    ])
+def test_move_exists_for(piece,x,y,directional,can_take):
+    move = Move(x,y,directional,can_take)
+    assert move in piece.moves, f"{move}, {directional}, {can_take} not found in {piece} moves."
+
+
